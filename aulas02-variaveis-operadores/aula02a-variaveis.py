a7a9360c615c44485d44d0ab29907194684a7c90
joaofreiitas <joao2007freitas@gmail.com>
2026-03-19 20:57:08 -0300

    Aula 2 - Váriaveis e Operadores.

---
+++ aulas02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print('ola mundo')
+print(7+4)
+print('7+4')
+print('7'+'4') #CONCATENAÇÃO DE STRINGS
+
+#comentários de uma linha
+
+
+# VARIAVEIS
+nome = 'Joao'
+print(nome)
+idade = 26 #int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+
+print(f"oiiiii, {nome}") #usar variaveis no print
+nome = input('digite o seu nome')
+idade = int(input('digite a sua idade'))
+peso = float(input('digite o seu peso'))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 1999
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f'Sua idade é: {idade}')
+
+
+
---
+++ aulas02-variaveis-operadores/desafios.py
@@ -0,0 +1,18 @@
+nome = input('qual o seu nome? ')
+print(f'Boas vindas, {nome}!')
+
+#-------------------------------------
+
+dia_nascimento = input('Em que dia voce nasceu? ')
+mes_nascimento = input('Em que mes voce nasceu? ')
+ano_nascimento = input('Em que ano voce nasceu? ')
+
+print(f'{nome}, voce nasceu no dia {dia_nascimento} de {mes_nascimento} de {ano_nascimento}')
+
+#------------------------------------
+num_1 = int(input('Digite um numero: '))
+num_2 = int(input('Digite outro numero'))
+soma = num_1 + num_2
+
+print(f'O resultado é igual a: {soma}')
+
